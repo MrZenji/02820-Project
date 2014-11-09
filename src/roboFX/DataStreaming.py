a44f1a05bc388c771656a0f180902a0a83b3a224
s103451 <s103451@student.dtu.dk>
2014-11-09 13:46:29 +0100

    Improvements to the DataStreamer

--- src/roboFX/DataStreaming.py
+++ src/roboFX/DataStreaming.py
@@ -3,6 +3,7 @@ Created on 28/10/2014
 
 @author: Kevin
 '''
+import json
 
 
 class DataStreaming(object):
@@ -12,11 +13,19 @@ class DataStreaming(object):
     use both live data as well as history data.
     '''
 
-    def __init__(self, params):
+    def __init__(self, filename="data.txt"):
         '''
-        Constructor
+        Load the data from a file
         '''
-        self.instrument = "EUR_USD"
+        jFile = open(filename)
+        self.data = json.load(jFile)
+        jFile.close()
+        self.count = 0
+        self.maxCount = len(self.data['candles'])
 
     def getData(self):
-        print "5-min worth of data"
+        self.count += 1
+        return self.data['candles'][self.count-1]
+
+    def reload(self):
+        self.count = 0
